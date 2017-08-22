4466e910bae73e683b2f38aa5fe198ce4d4f54ed
Brent-Milan <Bmilan24@gmail.com>
2017-08-21 21:24:33 -0400

    Babysitter kata started. TimeClockTest + TimeClock created.

---
+++ src/main/java/babysitter/kata/TimeClock.java
@@ -0,0 +1,5 @@
+package babysitter.kata;
+
+public class TimeClock {
+
+}
---
+++ src/test/java/babysitter/test/TimeClockTest.java
@@ -0,0 +1,24 @@
+package babysitter.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import babysitter.kata.TimeClock;
+
+public class TimeClockTest {
+
+	private TimeClock classUnderTest;
+	
+	@Before
+	public void setUp() throws Exception {
+		classUnderTest = new TimeClock();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
